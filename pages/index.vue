2bb279137278b629fc2ece25099e8053672d6b56
Heavenston <malolegendrelemaire@gmail.com>
2020-01-15 16:32:19 +0100

    added the favicon and made the text centered

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-container fill-height fluid >
-    <v-row >
-      <v-col>
+  <v-container fill-height fluid>
+    <v-row no-gutters justify="center">
+      <v-col class="flex-grow-0 text-no-wrap title">
         La lecture c'est cool est actuelement en beta
       </v-col>
     </v-row>
